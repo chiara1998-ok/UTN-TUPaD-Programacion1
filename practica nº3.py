abc72ed6e98034815c10faf23badcdd427785789
chiara1998-ok <diazchiara.1998@gmail.com>
2025-08-24 19:40:44 -0300

    segunda parte practica 3

--- practica nº3.py
+++ practica nº3.py
@@ -79,3 +79,27 @@ else:
     print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres")
 
 
+# 6) Teniendo en cuenta lo antes mencionado, escribir un programa que tome la lista 
+#numeros_aleatorios, calcule su moda, su mediana y su media y las compare para determinar si 
+#hay sesgo positivo, negativo o no hay sesgo. Imprimir el resultado por pantalla. 
+
+
+
+from statistics import mode, median, mean 
+
+import random 
+
+numeros_aleatorios = [random.randint(1, 100) for i in range(50)] 
+
+moda = mode(numeros_aleatorios)
+mediana = median(numeros_aleatorios)
+media = mean(numeros_aleatorios)
+print(numeros_aleatorios)
+if (media > mediana and mediana > moda):
+    print("Hay sesgo positivo")
+elif (media < mediana and mediana < moda ):
+    print("Hay sesgo negativo")
+else:
+    print("No hay sesgo")    
+
+
