fc67d9be335ede140d0f01b432a2eb516e7dd653
chiara1998-ok <diazchiara.1998@gmail.com>
2025-09-16 22:34:00 -0300

    practica 5

---
+++ Practico 5/Practica nº5.py
@@ -0,0 +1,20 @@
+1#
+
+list = [7,7,8,9,10,5,7,8,9,6]
+
+notaAlta = max(list)
+notaBaja = min(list)
+for cont in range(len(list)):
+
+    
+        sum(list)
+       
+
+
+
+print(list[0:len(list)])
+print(f"El promedio es {sum(list)/len(list)}")
+print(f"La nota mas alta es {notaAlta}")
+print(f"La nota mas baja es {notaBaja}")
+
+
--- ejercicios.py
+++ ejercicios.py
@@ -1,13 +1,3 @@
-#10
+#2
 
-num= int(input("Ingrese un numero entero "))
-invertido = 0
 
-while(num > 0):
-    digito = num % 10
-
-    invertido = (invertido * 10) + digito
-
-    num = num // 10
-    
-print(invertido)    
