60a5454be41fef9aab809f5645d17c76b6a6e1ae
chiara1998-ok <diazchiara.1998@gmail.com>
2025-09-19 17:33:34 -0300

    Ejercicios practica 5

--- Practico 5/Practica nº5.py
+++ Practico 5/Practica nº5.py
@@ -18,3 +18,23 @@ print(f"La nota mas alta es {notaAlta}")
 print(f"La nota mas baja es {notaBaja}")
 
 
+2#
+
+list = []
+
+for i in range(5):
+    producto = input("Ingrese un producto: ")
+
+    list.append(producto)
+    
+
+list.sort()   
+print(list[0:len(list)])     
+
+eliminar = input("Ingrese un producto a eliminar: ")
+if eliminar in list:
+    list.remove(eliminar)
+    print(f"Producto {eliminar} eliminado.")
+    print(f"Lista actualizada: {list[0:len(list)]}")
+else:
+    print(f"Producto {eliminar} no encontrado.")
--- ejercicios.py
+++ ejercicios.py
@@ -1,3 +1,22 @@
 #2
 
 
+
+list = []
+
+for i in range(5):
+    producto = input("Ingrese un producto: ")
+
+    list.append(producto)
+    
+
+list.sort()   
+print(list[0:len(list)])     
+
+eliminar = input("Ingrese un producto a eliminar: ")
+if eliminar in list:
+    list.remove(eliminar)
+    print(f"Producto {eliminar} eliminado.")
+    print(f"Lista actualizada: {list[0:len(list)]}")
+else:
+    print(f"Producto {eliminar} no encontrado.")
