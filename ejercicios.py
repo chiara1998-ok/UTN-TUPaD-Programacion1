723b2a3cd35143badea9b3d123b955ec0ff387e9
chiara1998-ok <diazchiara.1998@gmail.com>
2025-09-24 21:53:15 -0300

    ejercicio 9

--- ejercicios.py
+++ ejercicios.py
@@ -2,12 +2,12 @@
 
 
 
-lista= [
-     "-",  "-" , "-" ,
-     "-" , "-" , "-" ,
-     "-",  "-" , "-" 
-     ]
-
+lista= []
+for _ in range(3):
+    fila = []
+    for _ in range(3):
+        fila.append("-")
+    lista.append(fila)
 fila =int(input("Ingrese la fila (0, 1 o 2): "))
 columna =int(input("Ingrese la columna (0, 1 o 2): "))
 opcion = input("Ingrese X o O: ").upper()
